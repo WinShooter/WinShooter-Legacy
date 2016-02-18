88809ff8c6c1a485637c954f1ad192308e6fd947
smuda <john@ayoy.se>
2016-02-18 18:32:03 +0100

    Resharping.

--- Implementation/Common/Cache.cs
+++ Implementation/Common/Cache.cs
@@ -38,39 +38,57 @@ namespace Allberg.Shooter.Common
     internal class Cache
     {
         /// <summary>
-        /// Initiates the CCache
+        /// The startup resources.
+        /// </summary>
+        private readonly DSStartupResources startupResources;
+
+        /// <summary>
+        /// The local cache filename.
+        /// </summary>
+        private readonly string localCacheFilename;
+
+        /// <summary>
+        /// The local cache.
+        /// </summary>
+        private readonly DSLocalCache localCache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cache"/> class.
         /// </summary>
         internal Cache()
         {
-            //myInterface = callerInterface;
+            // myInterface = callerInterface;
             // Init cache from embedded file
-            _startup = new DSStartupResources();
+            this.startupResources = new DSStartupResources();
             /* TODO: Enable reading file.
              * _startup.ReadXml(
                 CEmbeddedResources.GetEmbeddedXmlFile(
                     "Allberg.Shooter.Common.DSStartupResources.xml"));*/
 
             // init cache from local file
-            _localCacheFilename = CreateLocalFilename();
-            _localCache = new DSLocalCache();
-            if (File.Exists(_localCacheFilename))
+            this.localCacheFilename = CreateLocalFilename();
+            this.localCache = new DSLocalCache();
+            if (File.Exists(this.localCacheFilename))
             {
-                _localCache.ReadXml(_localCacheFilename);
+                this.localCache.ReadXml(this.localCacheFilename);
             }
         }
 
-        private readonly DSStartupResources _startup;
-        private readonly string _localCacheFilename;
-        private readonly DSLocalCache _localCache;
-
-        #region Common
+        /// <summary>
+        /// The create local filename.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         private static string CreateLocalFilename()
         {
             var dir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
             dir += "\\Allberg\\WinShooter";
             if (!Directory.Exists(dir))
+            {
                 Directory.CreateDirectory(dir);
+            }
 
             return dir + "\\LocalCache.xml";
         }
@@ -80,9 +98,9 @@ namespace Allberg.Shooter.Common
         /// </summary>
         private void UpdateLocalCache()
         {
-            lock (_localCache)
+            lock (this.localCache)
             {
-                _localCache.WriteXml(_localCacheFilename);
+                this.localCache.WriteXml(this.localCacheFilename);
             }
         }
 
@@ -92,19 +110,17 @@ namespace Allberg.Shooter.Common
         /// <returns></returns>
         internal string GetCacheFileContent()
         {
-            lock (_localCache)
+            lock (this.localCache)
             {
                 using (var stream = new MemoryStream())
                 {
-                    _localCache.WriteXml(stream);
+                    this.localCache.WriteXml(stream);
 
                     return Encoding.UTF8.GetString(stream.ToArray());
                 }
             }
         }
-        #endregion
 
-        #region Shooter
         /// <summary>
         /// Returns the number of shooters in the local cache
         /// </summary>
@@ -115,26 +131,26 @@ namespace Allberg.Shooter.Common
         }
 
         /// <summary>
-        /// Returns a shooter with cardnr.
+        /// Returns a shooter with card number.
         /// </summary>
         /// <param name="cardNr">Card number</param>
-        /// <returns></returns>
+        /// <returns><see cref="Structs.Shooter"/></returns>
         internal Structs.Shooter GetShooter(string cardNr)
         {
             Structs.Shooter localFileShooter;
             try
             {
-                localFileShooter = GetShooterFromLocalFile(cardNr);
+                localFileShooter = this.GetShooterFromLocalFile(cardNr);
             }
             catch (CannotFindIdException)
             {
-                return GetShooterFromEmbeddedFile(cardNr);
+                return this.GetShooterFromEmbeddedFile(cardNr);
             }
 
             Structs.Shooter embeddedShooter;
             try
             {
-                embeddedShooter = GetShooterFromEmbeddedFile(cardNr);
+                embeddedShooter = this.GetShooterFromEmbeddedFile(cardNr);
             }
             catch (CannotFindIdException)
             {
@@ -142,89 +158,105 @@ namespace Allberg.Shooter.Common
             }
 
             if (localFileShooter.LastUpdate > embeddedShooter.LastUpdate)
+            {
                 return localFileShooter;
+            }
+
             return embeddedShooter;
         }
 
         /// <summary>
         /// Updates a shooter in the local cache file
         /// </summary>
-        /// <param name="shooter"></param>
+        /// <param name="shooter">The <see cref="Structs.Shooter"/></param>
         internal void UpdateShooterInCache(Structs.Shooter shooter)
         {
             if (int.Parse(shooter.CardNr) < 0)
+            {
                 return;
+            }
 
             var rows =
                 (DSLocalCache.ShootersRow[])
-                _localCache.Shooters.Select(
+                this.localCache.Shooters.Select(
                 "CardNr='" + shooter.CardNr + "'");
 
             DSLocalCache.ShootersRow row;
             if (rows.Length > 0)
+            {
                 row = rows[0];
+            }
             else
             {
-                row = _localCache.Shooters.NewShootersRow();
-                _localCache.Shooters.AddShootersRow(row);
+                row = this.localCache.Shooters.NewShootersRow();
+                this.localCache.Shooters.AddShootersRow(row);
             }
 
             bool updated = false;
 
-            #region check all attributes
-            if (row.IsCardnrNull() ||
-                row.Cardnr != shooter.CardNr)
+            // check all attributes
+            if (row.IsCardnrNull() || row.Cardnr != shooter.CardNr)
             {
                 row.Cardnr = shooter.CardNr;
                 updated = true;
             } 
-            if (row.IsClassNull() ||
-                row.Class != (int)shooter.Class)
+
+            if (row.IsClassNull() || row.Class != (int)shooter.Class)
             {
                 row.Class = (int)shooter.Class;
                 updated = true;
             }
-            if (row.IsClubIdNull() ||
-                row.ClubId != shooter.ClubId)
+
+            if (row.IsClubIdNull() || row.ClubId != shooter.ClubId)
             {
                 row.ClubId = shooter.ClubId;
                 updated = true;
             }
-            if (row.IsEmailNull() ||
-                row.Email != shooter.Email)
+
+            if (row.IsEmailNull() || row.Email != shooter.Email)
             {
                 row.Email = shooter.Email;
                 updated = true;
             }
-            if (row.IsGivennameNull() ||
-                row.Givenname != shooter.Givenname)
+
+            if (row.IsGivennameNull() || row.Givenname != shooter.Givenname)
             {
                 row.Givenname = shooter.Givenname;
                 updated = true;
             }
-            if (row.IsSurnameNull() ||
-                row.Surname != shooter.Surname)
+
+            if (row.IsSurnameNull() || row.Surname != shooter.Surname)
             {
                 row.Surname = shooter.Surname;
                 updated = true;
             }
-            #endregion
 
             if (updated)
             {
                 row.LastUpdate = DateTime.Now;
-                UpdateLocalCache();
+                this.UpdateLocalCache();
             }
         }
 
+        /// <summary>
+        /// Get the shooter from local file.
+        /// </summary>
+        /// <param name="cardNr">
+        /// The card nr.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Shooter"/>.
+        /// </returns>
         private Structs.Shooter GetShooterFromLocalFile(string cardNr)
         {
             var rows =
                 (DSLocalCache.ShootersRow[])
-                _localCache.Shooters.Select(
+                this.localCache.Shooters.Select(
                 "CardNr='" + cardNr + "'");
             if (rows.Length <= 0)
+            {
                 throw new CannotFindIdException("Could not find shooter");
+            }
 
             var shooter = new Structs.Shooter
                             {
@@ -234,21 +266,32 @@ namespace Allberg.Shooter.Common
                                 Givenname = rows[0].Givenname,
                                 Surname = rows[0].Surname,
                                 Automatic = true,
-                                Class = (Structs.ShootersClass) rows[0].Class,
+                                Class = (Structs.ShootersClass)rows[0].Class,
                                 LastUpdate = rows[0].LastUpdate
                             };
 
             return shooter;
         }
 
+        /// <summary>
+        /// Get the shooter from embedded file.
+        /// </summary>
+        /// <param name="cardNr">
+        /// The card nr.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Shooter"/>.
+        /// </returns>
         private Structs.Shooter GetShooterFromEmbeddedFile(string cardNr)
         {
             var rows =
                 (DSStartupResources.ShootersRow[])
-                _startup.Shooters.Select(
+                this.startupResources.Shooters.Select(
                     "CardNr='" + cardNr + "'");
             if (rows.Length <= 0)
+            {
                 throw new CannotFindIdException("Could not find shooter");
+            }
 
             var shooter = new Structs.Shooter
                             {
@@ -258,159 +301,13 @@ namespace Allberg.Shooter.Common
                                 Givenname = rows[0].Givenname,
                                 Surname = rows[0].Surname,
                                 Automatic = true,
-                                Class = (Structs.ShootersClass) rows[0].Class,
+                                Class = (Structs.ShootersClass)rows[0].Class,
                                 LastUpdate = rows[0].LastUpdate
                             };
 
             return shooter;
         }
-        #endregion
 
-        #region Weapon
-        /*/// <summary>
-        /// Returns a weapon with weaponid.
-        /// </summary>
-        /// <param name="WeaponId">Weapon id</param>
-        /// <returns></returns>
-        internal Structs.Weapon GetWeapon(string WeaponId)
-        {
-            Structs.Weapon localFileWeapon;
-            try
-            {
-                localFileWeapon = getWeaponFromLocalFile(WeaponId);
-            }
-            catch (CannotFindIdException)
-            {
-                return getWeaponFromEmbeddedFile(WeaponId);
-            }
-
-            Structs.Weapon embeddedWeapon;
-            try
-            {
-                embeddedWeapon = getWeaponFromEmbeddedFile(WeaponId);
-            }
-            catch (CannotFindIdException)
-            {
-                return localFileWeapon;
-            }
-
-            if (localFileWeapon.LastUpdate > embeddedWeapon.LastUpdate)
-                return localFileWeapon;
-            else
-                return embeddedWeapon;
-        }
-
-        internal void UpdateWeaponInCache(Structs.Weapon weapon)
-        {
-            DSLocalCache.WeaponsRow[] rows =
-                (DSLocalCache.WeaponsRow[])
-                localCache.Weapons.Select(
-                "CardNr='" + Weapon.CardNr + "'");
-
-            DSLocalCache.WeaponsRow row;
-            if (rows.Length > 0)
-                row = rows[0];
-            else
-            {
-                row = localCache.Weapons.NewWeaponsRow();
-                localCache.Weapons.AddWeaponsRow(row);
-            }
-
-            bool updated = false;
-
-            #region check all attributes
-            if (row.IsCardnrNull() ||
-                row.Cardnr != Weapon.CardNr)
-            {
-                row.Cardnr = Weapon.CardNr;
-                updated = true;
-            }
-            if (row.IsClassNull() ||
-                row.Class != (int)Weapon.Class)
-            {
-                row.Class = (int)Weapon.Class;
-                updated = true;
-            }
-            if (row.IsClubIdNull() ||
-                row.ClubId != Weapon.ClubId)
-            {
-                row.ClubId = Weapon.ClubId;
-                updated = true;
-            }
-            if (row.IsEmailNull() ||
-                row.Email != Weapon.Email)
-            {
-                row.Email = Weapon.Email;
-                updated = true;
-            }
-            if (row.IsGivennameNull() ||
-                row.Givenname != Weapon.Givenname)
-            {
-                row.Givenname = Weapon.Givenname;
-                updated = true;
-            }
-            if (row.IsSurnameNull() ||
-                row.Surname != Weapon.Surname)
-            {
-                row.Surname = Weapon.Surname;
-                updated = true;
-            }
-            #endregion
-
-            if (updated)
-            {
-                row.LastUpdate = DateTime.Now;
-                updateLocalCache();
-            }
-        }
-
-
-        private Structs.Weapon getWeaponFromLocalFile(string CardNr)
-        {
-            DSLocalCache.WeaponsRow[] rows =
-                (DSLocalCache.WeaponsRow[])
-                localCache.Weapons.Select(
-                "CardNr='" + CardNr + "'");
-            if (rows.Length <= 0)
-                throw new CannotFindIdException("Could not find Weapon");
-
-            Structs.Weapon Weapon = new Structs.Weapon();
-            Weapon.CardNr = rows[0].Cardnr;
-            Weapon.ClubId = rows[0].ClubId;
-            Weapon.Email = rows[0].Email;
-            Weapon.Givenname = rows[0].Givenname;
-            Weapon.Surname = rows[0].Surname;
-            Weapon.Automatic = true;
-            Weapon.Class = (Structs.WeaponsClass)rows[0].Class;
-            Weapon.LastUpdate = rows[0].LastUpdate;
-
-            return Weapon;
-        }
-
-        private Structs.Weapon getWeaponFromEmbeddedFile(string CardNr)
-        {
-            DSStartupResources.WeaponsRow[] rows =
-                (DSStartupResources.WeaponsRow[])
-                startup.Weapons.Select(
-                    "CardNr='" + CardNr + "'");
-            if (rows.Length <= 0)
-                throw new CannotFindIdException("Could not find Weapon");
-
-            Structs.Weapon Weapon = new Structs.Weapon();
-            shooter.CardNr = rows[0].Cardnr;
-            shooter.ClubId = rows[0].ClubId;
-            shooter.Email = rows[0].Email;
-            shooter.Givenname = rows[0].Givenname;
-            shooter.Surname = rows[0].Surname;
-            shooter.Automatic = true;
-            shooter.Class = (Structs.WeaponsClass)rows[0].Class;
-            shooter.LastUpdate = rows[0].LastUpdate;
-
-            return shooter;
-        }*/
-        #endregion
-        
-        #region Club
         /// <summary>
         /// Returns the number of clubs in the local cache
         /// </summary>
@@ -423,70 +320,69 @@ namespace Allberg.Shooter.Common
         /// <summary>
         /// Updates a club in cache
         /// </summary>
-        /// <param name="club"></param>
+        /// <param name="club">The <see cref="Structs.Club"/></param>
         internal void UpdateClubInCache(Structs.Club club)
         {
             var rows =
                 (DSLocalCache.ClubsRow[])
-                _localCache.Clubs.Select(
+                this.localCache.Clubs.Select(
                 "ClubId='" + club.ClubId + "'");
 
             DSLocalCache.ClubsRow row;
             if (rows.Length > 0)
+            {
                 row = rows[0];
+            }
             else
             {
-                row = _localCache.Clubs.NewClubsRow();
-                _localCache.Clubs.AddClubsRow(row);
+                row = this.localCache.Clubs.NewClubsRow();
+                this.localCache.Clubs.AddClubsRow(row);
             }
 
-            bool updated = false;
+            var updated = false;
 
-            #region check all attributes
-            if (row.IsClubIdNull() ||
-                row.ClubId != club.ClubId)
+            // check all attributes
+            if (row.IsClubIdNull() || row.ClubId != club.ClubId)
             {
                 row.ClubId = club.ClubId;
                 updated = true;
             }
-            if (row.IsCountryNull() ||
-                row.Country != club.Country)
+
+            if (row.IsCountryNull() || row.Country != club.Country)
             {
                 row.Country = club.Country;
                 updated = true;
             }
-            if (row.IsEmailNull() ||
-                row.Email != club.Email)
+
+            if (row.IsEmailNull() || row.Email != club.Email)
             {
                 row.Email = club.Email;
                 updated = true;
             }
-            if (row.IsNameNull() ||
-                row.Name != club.Name)
+
+            if (row.IsNameNull() || row.Name != club.Name)
             {
                 row.Name = club.Name;
                 updated = true;
             }
-            if (row.IsPlusgiroNull() ||
-                row.Plusgiro != club.Plusgiro)
+
+            if (row.IsPlusgiroNull() || row.Plusgiro != club.Plusgiro)
             {
                 row.Plusgiro = club.Plusgiro;
                 updated = true;
             }
-            if (row.IsBankgiroNull() ||
-                row.Bankgiro != club.Bankgiro)
+
+            if (row.IsBankgiroNull() || row.Bankgiro != club.Bankgiro)
             {
                 row.Bankgiro = club.Bankgiro;
                 updated = true;
             }
-            #endregion
 
             if (updated)
             {
                 row.LastUpdate = DateTime.Now;
-                UpdateLocalCache();
+                this.UpdateLocalCache();
             }
         }
-        #endregion
     }
 }
